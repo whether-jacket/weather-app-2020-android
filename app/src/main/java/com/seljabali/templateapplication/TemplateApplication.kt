77bdcaf80ec1ae5bdaa180c8afa0695d1be2b0c6
Sami Eljabali <seljabali@gmail.com>
2019-05-01 16:51:41 -0700

    add application class & dimens

---
+++ app/src/main/java/com/seljabali/templateapplication/TemplateApplication.kt
@@ -0,0 +1,14 @@
+package com.seljabali.templateapplication
+
+import android.app.Application
+
+class TemplateApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setupLibraries()
+    }
+
+    private fun setupLibraries() {
+    }
+}
