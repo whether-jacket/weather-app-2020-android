f2b1b883cae4754080251b7fd7615345bf706dcc
Sami Eljabali <seljabali@gmail.com>
2019-06-07 13:46:15 -0700

    class rename

--- app/src/main/java/com/seljabali/templateapplication/utilities/PhoneManufacturer.kt
+++ app/src/main/java/com/seljabali/templateapplication/utilities/PhoneManufacturer.kt
@@ -2,7 +2,7 @@ package com.seljabali.templateapplication.utilities
 
 import android.os.Build
 
-object Manufacturer {
+object PhoneManufacturer {
 
     private const val SAMSUNG_MANUFACTURER = "samsung"
     private const val SAMSUNG_S3_DEVICE_COMMON_PREFIX = "d2"
@@ -11,6 +11,5 @@ object Manufacturer {
 
     fun isSamsungGalaxyS3(): Boolean = Build.DEVICE.startsWith(SAMSUNG_S3_DEVICE_COMMON_PREFIX)
 
-    fun isSamsungS6(): Boolean = (Build.MODEL.contains("SM-G920F") || Build.MODEL.contains("SM-G920I")
-            || Build.MODEL.contains("SM-G920w8"))
+    fun isSamsungS6(): Boolean = Build.MODEL.contains("SM-G920F") || Build.MODEL.contains("SM-G920I") || Build.MODEL.contains("SM-G920w8")
 }
