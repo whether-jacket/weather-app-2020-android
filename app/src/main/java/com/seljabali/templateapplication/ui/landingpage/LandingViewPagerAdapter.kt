dd36d485f9396cb370960423ba15306325ff66ee
Sami Eljabali <seljabali@gmail.com>
2020-08-22 21:42:15 -0700

    update to viewpager2

--- app/src/main/java/com/seljabali/templateapplication/ui/landingpage/LandingViewPagerAdapter.kt
+++ app/src/main/java/com/seljabali/templateapplication/ui/landingpage/LandingViewPagerAdapter.kt
@@ -1,23 +1,21 @@
 package com.seljabali.templateapplication.ui.landingpage
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.seljabali.templateapplication.ui.cities.CitiesFragment
 import com.seljabali.templateapplication.ui.settings.SettingsFragment
 import com.seljabali.templateapplication.ui.weather.WeatherFragment
 
-class LandingViewPagerAdapter(fragmentManager: FragmentManager) :
-    FragmentStatePagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+class LandingViewPagerAdapter(fragment: Fragment) :
+    FragmentStateAdapter(fragment) {
 
-    override fun getCount(): Int = LandingPageTabs.values().size
+    override fun getItemCount(): Int = LandingPageTabs.values().size
 
-    override fun getItem(position: Int): Fragment =
+    override fun createFragment(position: Int): Fragment =
         when (position) {
             LandingPageTabs.HOME.ordinal -> WeatherFragment.newInstance()
             LandingPageTabs.CITIES.ordinal -> CitiesFragment.newInstance()
             LandingPageTabs.SETTINGS.ordinal -> SettingsFragment.newInstance()
             else -> throw IllegalStateException("Hit unknown tab position $position")
         }
-
 }
