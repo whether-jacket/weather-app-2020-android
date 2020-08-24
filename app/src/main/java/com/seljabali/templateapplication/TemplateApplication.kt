f79144d8555ef3c18c2e222c16639c34ddaaebba
Sami Eljabali <seljabali@gmail.com>
2020-08-24 05:00:55 -0700

    remove Hawk secure storage library

--- app/src/main/java/com/seljabali/templateapplication/TemplateApplication.kt
+++ app/src/main/java/com/seljabali/templateapplication/TemplateApplication.kt
@@ -1,37 +1,25 @@
 package com.seljabali.templateapplication
 
 import android.app.Application
-import android.content.Context
-import com.google.gson.Gson
-import com.orhanobut.hawk.GsonParser
-import com.orhanobut.hawk.Hawk
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
 import com.seljabali.core.modules.utilsModule
 import com.seljabali.database.databaseModule
 import com.seljabali.network.networkModule
-import org.koin.android.ext.android.inject
-import java.lang.ref.WeakReference
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class TemplateApplication: Application() {
 
-    companion object {
-        lateinit var instance: WeakReference<Context>
-    }
-
     private val modules = listOf(
         databaseModule,
         networkModule,
         utilsModule
     )
-    private val gson: Gson by inject()
 
     override fun onCreate() {
         super.onCreate()
-        instance = WeakReference(applicationContext)
         setupLibraries()
     }
 
@@ -42,8 +30,5 @@ class TemplateApplication: Application() {
             androidContext(this@TemplateApplication)
             modules(modules)
         }
-        Hawk.init(this)
-            .setParser(GsonParser(gson))
-            .build()
     }
 }
